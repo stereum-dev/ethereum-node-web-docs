518121396a85f0067a2e78e560cff49e5a25dd5f
montajebi <montajebi.ali@gmail.com>
2024-04-02 20:20:39 +0330

    REFACTOR: move to main branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  deploymentBranch: 'site',
+  deploymentBranch: 'main',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
