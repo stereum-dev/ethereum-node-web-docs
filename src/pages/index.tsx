5be005bf800faf461cd4e8d836e2516c213f0cfa
David Scheidl <david5scheidl@gmail.com>
2024-04-03 09:49:07 +0200

    Update homepage header link text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            🍄 Stereum Documentation 🍄
           </Link>
         </div>
       </div>
