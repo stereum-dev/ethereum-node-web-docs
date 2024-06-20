aa35e954fb75d37b04fe0c8c2925c0fba53747f8
daGscheid <david.scheidl@rocklogic.at>
2024-06-20 13:18:23 +0200

    Revert Changes (lol)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,17 +13,10 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-         <Link to="https://stereum.com" className={styles.logoLink}>
-           🍄 Stereum
-         </Link>
+         🍄 Stereum
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="/">
-            Home
-          </Link>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
