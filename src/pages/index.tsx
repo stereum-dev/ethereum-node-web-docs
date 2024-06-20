2d0b6639e95fdefeda07eb90d9ec2335290a73d7
daGscheid <david.scheidl@rocklogic.at>
2024-06-20 13:14:32 +0200

    Update Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,17 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-         🍄 Stereum
+         <Link to="https://stereum.com" className={styles.logoLink}>
+           🍄 Stereum
+         </Link>
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg"
+            to="/">
+            Home
+          </Link>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
